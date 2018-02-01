29b7d6f2c685f7b0cc9c3f5ada6e76c5824c5aae
lyccccx <623437080@qq.com>
2018-02-01 22:42:31 +0800

    t

--- MaterialDedit/QrCodeForm.Designer.cs
+++ MaterialDedit/QrCodeForm.Designer.cs
@@ -80,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 490);
+            this.ClientSize = new System.Drawing.Size(432, 490);
             this.Controls.Add(this.Paneltext);
             this.Controls.Add(this.panelQrCode);
             this.Controls.Add(this.panelQrCodePrint);
